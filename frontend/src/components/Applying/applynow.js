499e29d7145a7bc6a0524184cc40a780678419ef
muditbansal980 <muditban2008@gmail.com>
2026-01-05 11:40:38 +0530

    added active effect in the benefits filters

---
+++ frontend/src/components/Applying/applynow.js
@@ -0,0 +1,11 @@
+export default function applynow(){
+    return(
+        <div className="flex flex-col justify-center items-center w-full h-full">
+            <div className="bg-white rounded-[10px] w-[80%] h-[50%]">
+                <div className="heading text-[38px] font-bold">
+                    Sample Government Scheme 23
+                </div>
+            </div>
+        </div>
+    )
+}
