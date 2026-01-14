bc8e88e2f7103a68ca22af200bafce32c69620fb
muditbansal980 <muditban2008@gmail.com>
2026-01-14 10:18:18 +0530

    made a workinprogress component

---
+++ frontend/src/components/workinprogress/workinprogress.js
@@ -0,0 +1,7 @@
+export default function WorkInProgress() {
+    return (
+        <div className="h-[100vh] w-[100vw] flex justify-center items-center text-[24px] font-bold" >
+            Work In Progress ...
+        </div>
+    )
+}
